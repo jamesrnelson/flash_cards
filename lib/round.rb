53272b830c59caa58dd00c10f1ff5c73c00b6f89
JNelson <nelson.jimmy@gmail.com>
2018-01-28 19:59:13 -0700

    Round class with difficulties recording guesses.

---
+++ lib/round.rb
@@ -0,0 +1,32 @@
+require './lib/guess.rb'
+require './lib/deck.rb'
+require './lib/card.rb'
+require 'pry'
+
+class Round
+  attr_reader :deck, :guesses, :card_num, :player_response
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+    @card_num = 0
+    @player_response = []
+    @answered_correctly = 0
+  end
+
+  def current_card
+    deck.cards[@card_num]
+  end
+
+  def record_guess(guess)
+    guesses << guess = Guess.new(guess, current_card)
+    player_response << guess if guess.correct?
+    @answered_correctly += 1 if guess.correct?
+    @card_num += 1
+  end
+
+  def feedback
+    guess.feedback
+  end
+
+end
